f76c8944ab1a1fbfa79c7bb253b2d688630dc419
TabloNi <tabloni0719@gmail.com>
2021-01-26 08:25:55 +0900

    Add that

--- lib/main.dart
+++ lib/main.dart
@@ -7,10 +7,11 @@ void main() {
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Main',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -28,7 +29,7 @@ class MyApp extends StatelessWidget {
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flud'),
+      home: MyHomePage(title: 'sdfsdfs'),
     );
   }
 }
--- lib/simple_app.dart
+++ lib/simple_app.dart
@@ -3,21 +3,19 @@ import 'package:flutter/material.dart';
 void main() => runApp(MaterialApp(
       title: 'hello flutter',
       home: Scaffold(
-        appBar: AppBar(title: Text(' flu')),
+        appBar: AppBar(title: Text(' sdfsd')),
         body: _FirstStatefulWidget(),
       ),
     ));
 
-class _FirstStatefulWidget extends StatefulWidget{
-
+class _FirstStatefulWidget extends StatefulWidget {
   @override
   State createState() => _FirstStatefulWidgetState();
 }
 
-class _FirstStatefulWidgetState extends State<_FirstStatefulWidget>{
-
+class _FirstStatefulWidgetState extends State<_FirstStatefulWidget> {
   @override
   Widget build(BuildContext context) {
-      return Text('이것은 stateful 위젯임.');
+    return Text('이것은 stateful 위젯임.');
   }
 }
